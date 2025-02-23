5702368d8bfd6baed2e325429170a2d762c621d9
augustogabrielclaro <augustogabrielclaro@hotmail.com>
2025-02-22 23:48:56 -0300

    Finished?

--- src/jogodavelha/FXMLDocumentController.java
+++ src/jogodavelha/FXMLDocumentController.java
@@ -1,19 +1,10 @@
 package jogodavelha;
 
 import javafx.scene.input.MouseEvent;
-import java.util.Arrays;
 import java.net.URL;
-import java.util.HashSet;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.Set;
-import java.util.function.Predicate;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseButton;
@@ -23,6 +14,9 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private Pane janela;
+
+    @FXML
+    private Label jogador;
 
     @FXML
     private Label a1;
@@ -51,20 +45,43 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label c3;
 
-    static int nJogada = 1;
+    private Integer nJogada = 1;
 
-    
+    Alert mensagem = new Alert(Alert.AlertType.INFORMATION);
+
     @FXML
     public void velha() {
-        if (nJogada >= 9) {
-            mensagem.setTitle("Aviso");
-            mensagem.setHeaderText("Deu velha!");
-            mensagem.showAndWait();
-            nJogada = 1;
-            limparCampos();
+        if (nJogada > 9) {
+            if (!verificarVitoria()) {
+                mensagem.setTitle("Aviso");
+                mensagem.setHeaderText("Deu velha!");
+                mensagem.showAndWait();
+                limparCampos();
+            }
         }
     }
-    
+
+    @FXML
+    public boolean verificarVitoria() {
+        if (nJogada >= 5) {
+            if ((!a1.getText().isEmpty() && a1.getText().equals(a2.getText()) && a2.getText().equals(a3.getText()))
+                    || (!a1.getText().isEmpty() && a1.getText().equals(b1.getText()) && b1.getText().equals(c1.getText()))
+                    || (!a1.getText().isEmpty() && a1.getText().equals(b2.getText()) && b2.getText().equals(c3.getText()))
+                    || (!b1.getText().isEmpty() && b1.getText().equals(b2.getText()) && b2.getText().equals(b3.getText()))
+                    || (!c1.getText().isEmpty() && c1.getText().equals(c2.getText()) && c2.getText().equals(c3.getText()))
+                    || (!c1.getText().isEmpty() && c1.getText().equals(b2.getText()) && b2.getText().equals(a3.getText()))
+                    || (!a3.getText().isEmpty() && a3.getText().equals(b3.getText()) && b3.getText().equals(c3.getText()))
+                    || (!a2.getText().isEmpty() && a2.getText().equals(b2.getText()) && b2.getText().equals(c2.getText()))) {
+                mensagem.setTitle("Aviso");
+                mensagem.setHeaderText("Voce ganhou o jogo!");
+                mensagem.showAndWait();
+                limparCampos();
+                return true;
+            }
+        }
+        return false;
+    }
+
     @FXML
     public void limparCampos() {
         a1.setText("");
@@ -76,55 +93,46 @@ public class FXMLDocumentController implements Initializable {
         c1.setText("");
         c2.setText("");
         c3.setText("");
+        nJogada = 1;
+        jogador.setText("X");
+        jogador.setStyle("-fx-font-size: 15px; -fx-alignment: center;");
     }
 
-    Alert mensagem = new Alert(Alert.AlertType.INFORMATION);
-    
     @FXML
-    public void chamarLetra(Label label) {
+    public void colocarLetra(Label label) {
+        System.out.println(nJogada);
         if (nJogada % 2 != 0) {
             label.setText("X");
-            velha();
+            jogador.setText("O");
         } else {
             label.setText("O");
-            velha();
+            jogador.setText("X");
         }
         label.setStyle("-fx-font-size: 40px; -fx-alignment: center;");
+        jogador.setStyle("-fx-font-size: 15px; -fx-alignment: center;");
+        
         nJogada++;
-
-        if (nJogada > 5) {
-            if ((!a1.getText().isEmpty() && a1.getText().equals(a2.getText()) && a2.getText().equals(a3.getText()))
-            || (!a1.getText().isEmpty() && a1.getText().equals(b1.getText()) && b1.getText().equals(c1.getText()))
-            || (!a1.getText().isEmpty() && a1.getText().equals(b2.getText()) && b2.getText().equals(c3.getText()))
-            || (!b1.getText().isEmpty() && b1.getText().equals(b2.getText()) && b2.getText().equals(b3.getText()))
-            || (!c1.getText().isEmpty() && c1.getText().equals(c2.getText()) && c2.getText().equals(c3.getText()))
-            || (!c1.getText().isEmpty() && c1.getText().equals(b2.getText()) && b2.getText().equals(a3.getText()))
-            || (!a3.getText().isEmpty() && a3.getText().equals(b3.getText()) && b3.getText().equals(c3.getText()))
-            || (!a2.getText().isEmpty() && a2.getText().equals(b2.getText()) && b2.getText().equals(c2.getText()))) {
-                mensagem.setTitle("Aviso");
-                mensagem.setHeaderText("Voce ganhou o jogo!");
-                mensagem.showAndWait();
-                nJogada = 1;
-                limparCampos();
-            }
-        }
-
+        
+        if (!verificarVitoria()) {
+            velha();
+        }   
     }
 
     @FXML
-    private void colocarLetra(MouseEvent event) {
+    private void cliqueValido(MouseEvent event) {
         Label labelClicada = (Label) event.getSource();
 
         if (event.getButton() == MouseButton.PRIMARY) {
             if (labelClicada.getText().isEmpty()) {
-                chamarLetra(labelClicada);
+                colocarLetra(labelClicada);
             }
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        jogador.setText("PRIMEIRA JOGADA: X");
+        jogador.setStyle("-fx-font-size: 15px; -fx-alignment: center;");
     }
 
 }
